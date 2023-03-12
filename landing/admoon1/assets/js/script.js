020349371a593a1b8a220d1c190063326eda0325
Alireza Aghaei <appleiran66@gmail.com>
2023-03-12 23:37:38 +0330

    step 6

--- landing/admoon1/assets/js/script.js
+++ landing/admoon1/assets/js/script.js
@@ -1,29 +1,3 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
-
-
 // just get 11 number input
 var alireza = document.querySelectorAll(".alireza_validator input"),i;
 // Restricts input for the given textbox to the given inputFilter.
@@ -60,29 +34,36 @@ for (i = 0; i < alireza.length; ++i) {
 // });
 
 
-$(document).ready(function(){
-  $('.owl-carousel-index').owlCarousel({
-      rtl:true,
-      loop:true,
-      dots: false,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      nav: true,
-      margin: 80,
-      navText : ["<div class='owl-next'><img src='assets/images/sample/right-arrow.svg' width='54px'></div>","<div class='owl-prev'><img src='assets/images/sample/left-arrow.svg' width='54px'></div>"],
-      responsive:{
-        0:{
-            items:2,
-            nav:false,
-            margin: 80
-        },
-        600:{
-            items:3,
-            margin: 30
-        },
-        1000:{
-            items:4,
-        }
-       }
-  });
+//$(document).ready(function(){
+//  $('.owl-carousel-index').owlCarousel({
+//      rtl:true,
+//      loop:true,
+//      dots: false,
+//      autoplay: true,
+//      autoplayTimeout: 5000,
+//      nav: true,
+//      margin: 80,
+//      navText : ["<div class='owl-next'><img src='assets/images/sample/right-arrow.svg' width='54px'></div>","<div class='owl-prev'><img src='assets/images/sample/left-arrow.svg' width='54px'></div>"],
+//      responsive:{
+//        0:{
+//            items:2,
+//            nav:false,
+//            margin: 80
+//        },
+//        600:{
+//            items:3,
+//            margin: 30
+//        },
+//        1000:{
+//            items:4,
+//        }
+//       }
+//  });
+//});
+
+
+$("#buyButton").mouseover(function() {
+    $("div.circle-background").css("transform","scale(5)")
+}).mouseout(function() {
+   $("div.circle-background").css("transform","scale(1)")
 });
