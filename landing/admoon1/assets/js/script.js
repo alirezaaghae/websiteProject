4d276e96159e57a62456b155b25a7ab10e9edf2e
Alireza Aghaei <appleiran66@gmail.com>
2023-03-14 03:19:01 +0330

    Finished Front

--- landing/admoon1/assets/js/script.js
+++ landing/admoon1/assets/js/script.js
@@ -62,8 +62,124 @@ for (i = 0; i < alireza.length; ++i) {
 //});
 
 
-$("#buyButton").mouseover(function() {
+//admoon js 
+
+var itemSelected = '';
+var register = false;
+
+function ShowSelectItems(){
+    
+//  hide another
+    $("#formInputs").addClass("hidden").hasClass("show");
+    $("#formInputs").removeClass("show");
+    $("#registeredItems").addClass("hidden").hasClass("show");
+    $("#registeredItems").removeClass("show");
+//  show
+    $("#selectItems").addClass("show").not('.show');
+    $("#selectItems").removeClass("hidden");
+    
+    $("div.state.inquiry").removeClass("checked").hasClass("checked");
+    $("div.state.register").removeClass("checked").hasClass("checked");
+    return true;
+}
+
+function showFormInputs(){
+//  hide another
+    $("#selectItems").addClass("hidden").hasClass("show");
+    $("#selectItems").removeClass("show");
+    $("#registeredItems").addClass("hidden").hasClass("show");
+    $("#registeredItems").removeClass("show");
+//  show
+    $("#formInputs").addClass("show").not('.show');
+    $("#formInputs").removeClass("hidden");
+    
+    $("div.state.register").removeClass("checked").hasClass("checked");
+    $("div.state.inquiry").addClass("checked").not(".checked");
+    return true;
+}
+
+function showRegisteredItems(){
+//  hide
+    $("#selectItems").addClass("hidden").hasClass("show");
+    $("#selectItems").removeClass("show");
+    $("#formInputs").addClass("hidden").hasClass("show");
+    $("#formInputs").removeClass("show");
+//  show
+    $("#registeredItems").addClass("show").not('.show');
+    $("#registeredItems").removeClass("hidden");
+    
+    $("div.state.inquiry").addClass("checked").not(".checked");
+    $("div.state.register").addClass("checked").not(".checked");
+    return true;
+}
+
+$(document).ready(function(){
+    $("#buyButton").mouseover(function() {
     $("div.circle-background").css("transform","scale(5)")
-}).mouseout(function() {
-   $("div.circle-background").css("transform","scale(1)")
+    }).mouseout(function() {
+       $("div.circle-background").css("transform","scale(1)")
+    });
+
+
+    $("a.selectItem").click(function(){
+       var itemSelected = $(this).attr('id');
+
+       $("a.selectItem").removeClass("active");
+       $(this).addClass( "active" );
+       $("#next_button").addClass( "enable" ); 
+    });
+
+    $("#next_button").click(function(){
+       if ($("a.selectItem").hasClass("active")) {
+            if ($("#selectItems").hasClass("show")) {
+                $("#next_button").removeClass("enable");
+                return showFormInputs();
+                
+            }else if ($("#formInputs").hasClass("show") && register) {
+                $("#next_button").addClass("disable");
+                return showRegisteredItems();
+            }else {
+                $('input').each(function() {
+                    if(!$(this).val()){
+                        alert('لطفا تمام فیلد هارا پر کنید');
+                       return false;
+                    }else{
+//                        register = true;
+                    }
+                });
+            } if ($("#registeredItems").hasClass("show")) {
+                return null;
+            }
+    }
+    });
+    
+    $("#selectionCircle").click(function(){
+        if ($('#formInputs').hasClass('show')){
+            return ShowSelectItems();
+        } else{
+            return false;
+        }
+    });
+    $("#inquiryCircle").click(function(){
+        if ($('#formInputs').hasClass('show')){
+            return showFormInputs();
+        } else{
+            return false;
+        }
+            
+    });
+    
 });
+
+function enableNextButton(){
+    $('input').each(function() {
+        if(!$(this).val()){
+            return false;
+        }else{
+            register = true;
+            $("#next_button").removeClass("disable");
+            $("#next_button").addClass("enable");
+        }
+     });
+    
+}
